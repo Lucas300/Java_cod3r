63932d989d6d974efd449e441cecbeb3cf6cee31
Lucas Daniel <ludaniel.sd@gmail.com>
2023-11-17 20:17:40 -0300

    No projeto exercício podemos ver as 3 formas de se comentar um código, no exercício clinica e faculdade podemos ver como se organiza um projeto java usando pacotes e classes

---
+++ 01_Fundamentos_java/exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		//uma sentença de código termina com ;
+		System.out.println("Primeiro programa parte #01!");
+		System.out.println("Primeiro programa parte #02!");
+		/*
+		 * 
+		Para comentar a barra inteira basta:
+		CTRL + /   
+		
+		*/
+	}
+}
---
+++ 01_Fundamentos_java/faculdade/src/aministracao/Agenda.java
@@ -0,0 +1,5 @@
+package aministracao;
+
+public class Agenda {
+
+}
