9613aec87b2dfe5ce2d9182399a4ad3b2e12df25
Lucas Daniel <ludaniel.sd@gmail.com>
2023-11-17 22:03:21 -0300

    Inferencia, podemos declarar uma variavel como var NomeDaVariavel e ela recebera um tipo de variavel com base no valor que recebeu

---
+++ 01_Fundamentos_java/exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,26 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		/*
+		Inferencia, podemos 
+		declarar uma variavel como var NomeDaVariavel
+		e ela recebera um tipo de variavel com base no valor que receber
+		*/
+		
+		double a = 4.5;
+		System.out.println("a");
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "texto";
+		System.out.println(c);
+		
+		c = "outro texto" ;
+		System.out.println(c);
+		
+		
+	
+	}
+}
