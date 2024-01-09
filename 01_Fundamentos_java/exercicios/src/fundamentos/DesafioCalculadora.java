907de23efda1b349e785f344dc3c7e413563b6d5
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-08 23:41:49 -0300

    Resolvendo o primeiro exercicio do fim de modulo de fundamentos, convertendo uma temperatura de fahrenheit para celcius

--- 01_Fundamentos_java/exercicios/src/fundamentos/DesafioCalculadora.java
+++ 01_Fundamentos_java/exercicios/src/fundamentos/DesafioCalculadora.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class DesafioCalculadora {
 	public static void main(String[] args) {
+		//entrada
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Digite o 1º Numero: ");
 		double n1 = entrada.nextDouble();
@@ -12,13 +13,15 @@ public class DesafioCalculadora {
 		System.out.println("Digite a operação + - * / %");
 		String operacao = entrada.next();
 		
+		//Lógica
 		double resultado = "+".equals(operacao)?n1 + n2:0;
 		resultado = "-".equals(operacao)?n1-n2:resultado;
 		resultado = "*".equals(operacao)?n1*n2:resultado;
 		resultado = "/".equals(operacao)?n1/n2:resultado;
 		resultado = "%".equals(operacao)?n1%n2:resultado;
 		
-		System.out.println(resultado);	
+		//Saída
+		System.out.printf("%.2f %s %.2f = %.2f",n1 , operacao , n2 , resultado );
 		entrada.close();
 	}
 
