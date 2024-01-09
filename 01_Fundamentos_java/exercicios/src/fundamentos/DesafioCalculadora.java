8801d8d4050ac0c63ab436eff50dd3b6fc315ce3
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-08 23:20:13 -0300

    Resolvendo o Desafio calculadora sem usar o IF ELSE

---
+++ 01_Fundamentos_java/exercicios/src/fundamentos/DesafioCalculadora.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class DesafioCalculadora {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite o 1º Numero: ");
+		double n1 = entrada.nextDouble();
+		System.out.println("Digite o 2º Numero: ");
+		double n2 = entrada.nextDouble();
+		System.out.println("Digite a operação + - * / %");
+		String operacao = entrada.next();
+		
+		double resultado = "+".equals(operacao)?n1 + n2:0;
+		resultado = "-".equals(operacao)?n1-n2:resultado;
+		resultado = "*".equals(operacao)?n1*n2:resultado;
+		resultado = "/".equals(operacao)?n1/n2:resultado;
+		resultado = "%".equals(operacao)?n1%n2:resultado;
+		
+		System.out.println(resultado);	
+		entrada.close();
+	}
+
+}
--- 01_Fundamentos_java/exercicios/src/fundamentos/operadores/Ternario.java
+++ 01_Fundamentos_java/exercicios/src/fundamentos/operadores/Ternario.java
@@ -5,11 +5,10 @@ public class Ternario {
 	public static void main(String[] args) {
 		double media = 7.4;
 		
-		String resultadoFinal = media>= 7.0 ? 
-				"aprovado" : "em recuperação";
+		String resultadoFinal = media>= 7.0 ? "aprovado" : "em recuperação";
 		
 		System.out.println("O aluno está "+ resultadoFinal);
-
+		
 	}
 
 }
