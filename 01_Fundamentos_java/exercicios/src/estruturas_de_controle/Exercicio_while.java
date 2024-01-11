b02f8011e610305e24d300bb1289457070813248
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-10 23:36:56 -0300

    Aprendendo sobre While indeterminado , ele repete até a condição retornar falso e sair do laço

---
+++ 01_Fundamentos_java/exercicios/src/estruturas_de_controle/Exercicio_while.java
@@ -0,0 +1,17 @@
+package estruturas_de_controle;
+
+import java.util.Scanner;
+
+public class Exercicio_while {
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		String palavra = "";
+		while (!palavra.equalsIgnoreCase("sair")) {
+			System.out.print("Digite alguma palavra: ");
+			palavra = entrada.next();
+		}
+		entrada.close();
+	}
+}
--- 01_Fundamentos_java/exercicios/src/fundamentos/operadores/Operadores.java
+++ 01_Fundamentos_java/exercicios/src/fundamentos/operadores/Operadores.java
@@ -12,7 +12,7 @@ public class Operadores {
 		
 		//Aritimeticos * / % + -
 		//Relacionais  && || ^ !
-		//Lógicos      > < =
+		//Lógicos      > < == 
 		//Atribuição  ++ =
 
 	}
