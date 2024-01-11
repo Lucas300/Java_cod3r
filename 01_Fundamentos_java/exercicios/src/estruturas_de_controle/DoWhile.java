99078f4be97631c27e38539ee6d9c4aa3d021c34
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-10 23:59:15 -0300

    Aprendendo sobre do while, ele executa uma vez o código antes de checar se a condição é verdadeira ou falsa

---
+++ 01_Fundamentos_java/exercicios/src/estruturas_de_controle/DoWhile.java
@@ -0,0 +1,25 @@
+package estruturas_de_controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		//if(....){ cod }
+		//while(....){  cod }
+		//for(..;..;..){ cod }
+		
+		//do{  }while(.....); ela entrará pelo menos uma vez antes da condição
+		Scanner entrada = new Scanner(System.in);
+		String texto = "";	
+		
+		do {
+				System.out.println("Digite as palavras mágicas..");
+				System.out.print("Quer Sair?");
+				texto = entrada.next();
+		}while(!texto.equalsIgnoreCase("sair"));
+		System.out.println("Obrigado");
+		entrada.close();
+	}
+
+}
