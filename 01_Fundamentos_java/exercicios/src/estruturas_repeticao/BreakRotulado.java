e72daeb55e429e1eaf6839de95becac0c1039da2
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-12 23:22:20 -0300

    Aprendendo sonbre o Break Rotulado , nomeamos um for e podemos colocar um if com o break na aplicação para interrompermos o for nomeado

---
+++ 01_Fundamentos_java/exercicios/src/estruturas_repeticao/BreakRotulado.java
@@ -0,0 +1,20 @@
+package estruturas_repeticao;
+
+public class BreakRotulado {
+
+	public static void main(String[] args) {
+		//dando um nome para o laço
+		externo:for (int i = 0; i < 3; i++) {
+			interno:for (int j = 0; j < 3; j++) {
+						if (i == 1) {
+						//to falando que quero pular o laço externo
+							break interno;	
+						}	
+					System.out.printf("[%d %d]",i , j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
