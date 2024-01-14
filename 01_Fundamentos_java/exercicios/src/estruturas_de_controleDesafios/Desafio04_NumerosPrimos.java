09d0f43a5b3b1896daa93573af6151a6fcbe5e81
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-13 22:29:00 -0300

    Desafio 04 do fim do modulo de estruturas de controle, O usuário digitara um número e o programa falara se o número é primo

---
+++ 01_Fundamentos_java/exercicios/src/estruturas_de_controleDesafios/Desafio04_NumerosPrimos.java
@@ -0,0 +1,30 @@
+package estruturas_de_controleDesafios;
+
+import java.util.Scanner;
+
+public class Desafio04_NumerosPrimos {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+			System.out.print("Digite um Número:");
+			int n = entrada.nextInt(); 
+			if((n%n==0 && n%1==0 && !(n%2==0)&& !(n%3==0) && !(n%5==0) && !(n%7==0) && !(n%9==0))||(n== 2 ||  n ==3 || n==5 || n==7 )) {
+					System.out.printf("O numero %d é primo",n);
+				 
+			}else {
+				System.out.printf("O numero %d não é primo",n);
+			}
+			
+//			if (n>1 && n%n==0 && !(n%5==0) && !(n%7==0) || n==5) {
+//				if(n ==2 || !(n%2==0) || !(n%3==0) || n ==3 || n==7) {
+//					System.out.printf("O numero %d é primo:",n);					
+//				}
+//			}
+//			else {
+//				System.out.printf("O numero %d não é primo:",n);
+//			}
+		entrada.close();
+
+	}
+
+}
