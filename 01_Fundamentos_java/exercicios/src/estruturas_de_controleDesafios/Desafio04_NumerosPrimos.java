2644a88845b172860c47943cd21ac96917d9f98e
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-13 23:05:36 -0300

    Desafio 05 do fim do modulo de estruturas de controle, Reproduzindo o exercicio4 Usando Switch case. O usuário digitara um número e o programa falara se o número é primo

--- 01_Fundamentos_java/exercicios/src/estruturas_de_controleDesafios/Desafio04_NumerosPrimos.java
+++ 01_Fundamentos_java/exercicios/src/estruturas_de_controleDesafios/Desafio04_NumerosPrimos.java
@@ -9,20 +9,11 @@ public class Desafio04_NumerosPrimos {
 			System.out.print("Digite um Número:");
 			int n = entrada.nextInt(); 
 			if((n%n==0 && n%1==0 && !(n%2==0)&& !(n%3==0) && !(n%5==0) && !(n%7==0) && !(n%9==0))||(n== 2 ||  n ==3 || n==5 || n==7 )) {
-					System.out.printf("O numero %d é primo",n);
+					System.out.printf("O número %d é primo",n);
 				 
 			}else {
-				System.out.printf("O numero %d não é primo",n);
+				System.out.printf("O número %d não é primo",n);
 			}
-			
-//			if (n>1 && n%n==0 && !(n%5==0) && !(n%7==0) || n==5) {
-//				if(n ==2 || !(n%2==0) || !(n%3==0) || n ==3 || n==7) {
-//					System.out.printf("O numero %d é primo:",n);					
-//				}
-//			}
-//			else {
-//				System.out.printf("O numero %d não é primo:",n);
-//			}
 		entrada.close();
 
 	}
---
+++ 01_Fundamentos_java/exercicios/src/estruturas_de_controleDesafios/Desafio05_AdivinheONumero.java
@@ -0,0 +1,33 @@
+package estruturas_de_controleDesafios;
+
+import java.util.Scanner;
+
+public class Desafio05_AdivinheONumero {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		//(n%n==0 && n%1==0 && !(n%2==0)&& !(n%3==0) && !(n%5==0) && !(n%7==0) && !(n%9==0))||(n== 2 ||  n ==3 || n==5 || n==7 )
+		
+		System.out.println("Digite um número");
+		int n = entrada.nextInt();
+		int n1;
+		boolean b = ((n%n==0 && n%1==0 && !(n%2==0)&& !(n%3==0) && !(n%5==0) && !(n%7==0) && !(n%9==0))||(n== 2 ||  n ==3 || n==5 || n==7 ));
+		
+		if(b) {
+			n1 = 1;
+		}else {
+			n1=0;
+		}
+						
+		switch(n1){
+		case 0:
+			System.out.printf("O número %d não é Primo",n);
+			break;
+		case 1:
+			System.out.printf("O número %d é Primo",n);
+			break;
+		}
+		
+		entrada.close();
+	}
+
+}
