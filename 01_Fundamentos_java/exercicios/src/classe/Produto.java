3de797dcb5fe529594c45d2b86630cad5acf059f
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-20 23:19:32 -0300

    Aprendendo sobre metodos construtores, que servem para criar abjetos de uma classe, seja recebendo parâmetros ou não

--- 01_Fundamentos_java/exercicios/src/classe/Produto.java
+++ 01_Fundamentos_java/exercicios/src/classe/Produto.java
@@ -4,6 +4,18 @@ public class Produto {
 	String nome;
 	double preco;
 	double desconto;
+	Produto(){
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial,double precoDesconto){
+		nome = nomeInicial;
+		preco = precoInicial;
+		desconto = precoDesconto;
+		
+	}
+	
+	
 	//podemos ter metodos com o mesmo nome mas com parâmetros diferentes
 	double precoComDesconto() { //parâmetro só se for acessar atributos de fora da classe
 		return preco*(1-desconto);
--- 01_Fundamentos_java/exercicios/src/classe/ProdutoTeste.java
+++ 01_Fundamentos_java/exercicios/src/classe/ProdutoTeste.java
@@ -3,11 +3,10 @@ package classe;
 public class ProdutoTeste {
 
 	public static void main(String[] args) {
-		Produto p1 = new Produto();
-		p1.nome = "Notebook";
-		p1.preco = 3256.89;
-		p1.desconto = 0.25;
+		//construtor com parâmetros
+		Produto p1 = new Produto("Notebook",4356.89,0.25);
 		
+		//contrutor padrão, sem receber parâmetros
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
@@ -15,10 +14,11 @@ public class ProdutoTeste {
 		
 		System.out.println(p1.nome);
 		System.out.println(p2.nome);
+
 		
 		double precofinal1 = p1.precoComDesconto();
-		double precofinal2 = p2.precoComDesconto(0.2);
-		double mediaCarrinho = (p1.preco + p2.preco)/2;
+		double precofinal2 = p2.precoComDesconto(0.1);
+		double mediaCarrinho = (precofinal1 + precofinal2 )/2;
 		System.out.printf("Média do carrinho = R$%.2f ", mediaCarrinho);
 		
 	}
