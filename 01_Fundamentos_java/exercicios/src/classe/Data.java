9ede7ec19b40aba88865f5333002af312eb1eb95
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-22 22:47:06 -0300

    Variáveis Locais: É importante que a definição das variáveis e constantes seja junto com os atributos da classe. as variáveis criadas dentro dos métodos só podem ser acessadas dentro do próprio método

--- 01_Fundamentos_java/exercicios/src/classe/Data.java
+++ 01_Fundamentos_java/exercicios/src/classe/Data.java
@@ -4,21 +4,26 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
+	//podemos usar o this nos para chamar métodos como um construtor chamando outro, para acessar um atributo,  this. para acessar Metodo, 
 	Data(){
-		 dia = 1;
-		 mes = 1;
-		 ano = 1970;
+		/*dia = 1;
+		  mes = 1; 
+		  ano = 1970;
+		  */
+		this(1,1,1970);
+		//this só pode dentro do metodo construtor
 	}
-	Data(int diaInicial, int mesInicial, int anoInicial){
-		 dia = diaInicial;
-		 mes = mesInicial;
-		 ano = anoInicial;
+	// usamos o this para diferenciar o parametro do contrutor
+	// e os atributos do objeto, this.dia é faz parte do parametro do método  
+	Data(int dia, int mes, int ano){
+		 this.dia = dia;
+		 this.mes = mes;
+		 this.ano = ano;
 	}
 
 	String obterDataFormatada() {
-		String formato = (String)(dia+"/"+mes+"/"+ano);
-		//return String.format("%d/%d/%d", dia,mes,ano);
-		return formato;
+		final String formato = "%d/%d/%d";
+		return String.format(formato,this.dia,mes,ano);
 		}
 	//não é viável mas da pra usar um método dentro de outro
 	void ImprimirDataFormatada() {
