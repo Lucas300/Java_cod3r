887f640e16ebb4d057405ed5a6bc743db0b48e6e
Lucas Daniel <ludaniel.sd@gmail.com>
2024-01-27 00:03:10 -0300

    Corrigindo o desafio da maneira correta

--- 01_Fundamentos_java/exercicios/src/arrays/DesafioArray.java
+++ 01_Fundamentos_java/exercicios/src/arrays/DesafioArray.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class DesafioArray {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
+		//O usuário ira digitar a quantidade de notas 
 		
 		System.out.print("Quantas notas deseja digitar?:");
 		int quantidade = entrada.nextInt();
-		quantidade -= 1;
 		double notas[] = new double[quantidade];
 		double total = 0;
 		
-		for (int i = 0; i <= notas.length; i++) {
+		for (int i = 0; i <= (quantidade-1); i++) {
 			System.out.println("Digite "+(i+1)+"º nota:");
 			notas[i] = entrada.nextDouble();
 			total += notas[i];
@@ -20,7 +20,7 @@ public class DesafioArray {
 		
 		
 		
-		System.out.println("A média é: "+total);
+		System.out.println("A média é: "+total/quantidade);
 		
 		entrada.close();
 
